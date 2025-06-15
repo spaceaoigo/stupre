e7fa63735cce9ff6593e22fda57badef4a8fca6f
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 22:49:02 +0900

    Update urls.py

--- records/urls.py
+++ records/urls.py
@@ -1,1 +1,17 @@
+from django.urls import path
+from .views import RecordListView, RecordCreateView
 
+app_name = 'records'
+
+urlpatterns = [
+    path('', RecordListView.as_view(), name='record_list'),
+    path('create/', RecordCreateView.as_view(), name='record_create'),
+]from django.urls import path
+from .views import RecordListView, RecordCreateView
+
+app_name = 'records'
+
+urlpatterns = [
+    path('', RecordListView.as_view(), name='record_list'),
+    path('create/', RecordCreateView.as_view(), name='record_create'),
+]
