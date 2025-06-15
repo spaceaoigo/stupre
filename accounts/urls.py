64b3e481c025d52f730dc3679feb4e219cc48829
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 23:40:20 +0900

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,17 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
-from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CustomUserCreationForm
+from django.urls import path
+from .views import SignUpView, ProfileView
+from django.contrib.auth import views as auth_views
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('accounts:login')
-    template_name = 'accounts/signup.html'
+app_name = 'accounts'
 
-class ProfileView(LoginRequiredMixin, DetailView):
-    model = get_user_model()
-    template_name = 'accounts/profile.html'
-    def get_object(self):
-        return self.request.user
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    # B-3: パスワードリセット機能
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), name='password_reset_complete'),
+]
