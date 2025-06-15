b979abb6d10394c7a969d498009240b5d874342a
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 23:01:00 +0900

    Update urls.py

--- records/urls.py
+++ records/urls.py
@@ -1,44 +1,18 @@
-# (既存のimportの下に追加)
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, redirect
-from django.views import View
-from .models import Record, Like, Comment # LikeとCommentを追加
+from django.urls import path
+from .views import (
+    RecordListView, 
+    RecordCreateView, 
+    RecordDetailView, 
+    LikeView, 
+    CommentCreateView
+)
 
-# (RecordListView, RecordCreateViewの下に追記)
-class LikeView(LoginRequiredMixin, View):
-    def post(self, request, *args, **kwargs):
-        record = get_object_or_404(Record, pk=self.kwargs['pk'])
-        like, created = Like.objects.get_or_create(user=request.user, record=record)
+app_name = 'records'
 
-        if not created:
-            like.delete()
-            liked = False
-        else:
-            liked = True
-
-        return JsonResponse({'liked': liked, 'count': record.likes.count()})
-
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    fields = ['text']
-    template_name = 'records/record_detail.html' # コメントは詳細ページで行う
-
-    def form_valid(self, form):
-        record = get_object_or_404(Record, pk=self.kwargs['pk'])
-        form.instance.user = self.request.user
-        form.instance.record = record
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('records:record_detail', kwargs={'pk': self.kwargs['pk']})
-
-class RecordDetailView(LoginRequiredMixin, DetailView):
-    model = Record
-    template_name = 'records/record_detail.html'
-    context_object_name = 'record'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.all()
-        context['comment_form'] = CommentForm() # CommentFormを定義する必要がある
-        return context
+urlpatterns = [
+    path('', RecordListView.as_view(), name='record_list'),
+    path('create/', RecordCreateView.as_view(), name='record_create'),
+    path('record/<int:pk>/', RecordDetailView.as_view(), name='record_detail'),
+    path('record/<int:pk>/like/', LikeView.as_view(), name='like'),
+    path('record/<int:pk>/comment/', CommentCreateView.as_view(), name='comment_create'),
+]
