926b27b40a937b4a7522cc69d8ae04a11ce39ff9
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 22:46:28 +0900

    Create admin.py

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'level', 'target_school', 'is_staff']
+
+admin.site.register(CustomUser, CustomUserAdmin)
