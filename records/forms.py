b7aafc2f26dc0ddcf1543214c212e179a420600c
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 23:00:00 +0900

    Update forms.py

--- records/forms.py
+++ records/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Record
+from .models import Record, Comment
 
 class RecordForm(forms.ModelForm):
     class Meta:
@@ -9,4 +9,15 @@ class RecordForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': '何を勉強しましたか？ (例: 数学I P.10-15)'}),
             'concentration': forms.NumberInput(attrs={'type': 'range', 'min': '1', 'max': '10', 'class': 'form-range'}),
             'memo': forms.Textarea(attrs={'rows': 2, 'placeholder': '感想や次回の課題など'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 2, 'placeholder': 'コメントを追加...'}),
+        }
+        labels = {
+            'text': '', # フォームのラベルを非表示にする
         }
