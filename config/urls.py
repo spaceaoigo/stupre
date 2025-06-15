ac06ce9d723234a2c080952cc0575c4690d19de1
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 22:49:40 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('posts.urls')),
-    
-    # PWA用のURLを追加
+    path('', include('records.urls')), # recordsアプリをルートURLに設定
     path('', include('pwa.urls')),
 ]
 
-# メディアファイル配信のための設定
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
