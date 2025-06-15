53050fea665d77476567a59c87184b94ea591d71
spaceaoigo <spaceaoigo@gmail.com>
2025-06-15 22:46:48 +0900

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,16 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CustomUserCreationForm
 
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('accounts:login')
+    template_name = 'accounts/signup.html'
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'accounts/profile.html'
+    def get_object(self):
+        return self.request.user
